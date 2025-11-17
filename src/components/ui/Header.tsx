e058afa3e29262690bdf3e3ab9e005a6f73c3296
Lorenzo Leão <lrzo.leao@gmail.com>
2025-11-17 19:26:51 -0300

    ajuste do deploy 5.

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -2,33 +2,36 @@
 import Image from "next/image"
 import Link from "next/link"
 
-export default function Hearder() {
+export default function Header() {
     return (
 <div className="flex items-center justify-center md:justify-between w-full max-w-[1042px] mx-auto bg-[#121212] border border-[#303030] rounded-xl px-4 py-2 mt-2">
-  {/* Logo apenas desktop */}
+  
   <div className="hidden md:flex items-center gap-2">
-    <Image src="/list-todo 1.svg" alt="" width={32} height={32} />
+    <Image src="/list-todo-1.svg" alt="" width={32} height={32} />
     <Image src="/FOCO.svg" alt="" width={91} height={20} />
   </div>
+
   <nav className="flex items-center justify-center md:justify-start gap-5 w-fit md:w-auto p-0 scale-90 md:scale-100 md:p-2.5">
+
     <Link href="/projects">
       <button className="flex w-8 h-8 items-center justify-center rounded-full hover:bg-yellow-400/10 transition">
-        <Image src="/frame6.svg" width={32} height={32} alt="Menu" />
+        <Image src="/frame6.svg" width={32} height={32} alt="projetos" />
       </button>
     </Link>
+
     <Link href="/tasks">
       <button className="flex w-8 h-8 items-center justify-center rounded-full hover:bg-yellow-400/10 transition">
-        <Image src="/frame4.svg" width={18} height={18} alt="" className="pointer-events-none" />
+        <Image src="/frame4.svg" width={18} height={18} alt="tarefas" className="pointer-events-none" />
       </button>
     </Link>
+
     <Link href="/profile">
       <button className="flex w-8 h-8 items-center justify-center rounded-full hover:bg-yellow-400/10 transition">
-        <Image src="/frame7.svg" width={32} height={32} alt="Perfil" />
+        <Image src="/frame7.svg" width={32} height={32} alt="perfil" />
       </button>
     </Link>
+
   </nav>
 </div>
-
     )
 }
-
